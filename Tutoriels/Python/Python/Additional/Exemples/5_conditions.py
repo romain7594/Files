c522fc9ee6ddee6d633f5e213355e7bb49e0631d
romain7594 <ultimategseed2@hotmail.fr>
2018-10-15 21:39:16 +0200

    Create 5_conditions.py

---
+++ Tutoriels/Python/Python/Additional/Exemples/5_conditions.py
@@ -0,0 +1,39 @@
+##### dia 151 #####
+nombre = 7
+
+if nombre > 5 :
+	print "Le nombre est supérieur à 5"
+
+else :
+	print "Le nombre est inférieur à 5"
+
+
+##### dia 154 #####
+nombre1 = 7
+nombre2 = 5
+
+if nombre1 > 3 :
+	if nombre2 > 3 :
+		print "Les nombres sont supérieurs à 3"
+		
+else :
+	print "Au moins un des deux nombres est inférieur à 3"
+	
+#if nombre1 > 3 and nombre2 > 3 :
+	#print "Les deux nombres sont supérieurs à 3"
+#else :
+	#print "Au moins un des deux nombres est inférieur à 3"
+	
+
+##### dia 158 #####
+nombre1 = 7
+nombre2 = 5
+
+if nombre1+nombre2 > 20 :
+	print "La somme des deux nombres est supérieur à 20"
+
+elif nombre1+nombre2 == 12 :
+	print "La somme des deux nombres est égale à 12"
+
+else :
+	print "La somme des deux nombres est inférieur à 20 et n'est pas égale à 12"
