814c43ab3d2275a8bc814221237a4f47bb16194a
romain7594 <ultimategseed2@hotmail.fr>
2018-10-15 20:31:43 +0200

    Update 1_variables.py

--- Tutoriels/Python/Python/Additional/Exemples/1_variables.py
+++ Tutoriels/Python/Python/Additional/Exemples/1_variables.py
@@ -1,11 +1,11 @@
-### dia 21
+##### dia 21
 JeMetsDansLaVariableLeChiffreUn = 1
 JeContientUneChaineDeCaracteres = 'tototatatititutu'
 
 
-### dia 23
+##### dia 23
 mot1, mot2, mot3 = 'Coucou', 'ça', 'va'
 
 
-### dia 24
+##### dia 24
 phrase = 'Coucou', 'ça', 'va'
