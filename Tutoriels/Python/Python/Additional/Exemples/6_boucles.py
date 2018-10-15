6a036a65d78764c85de5c6bfc68137344b4082da
romain7594 <ultimategseed2@hotmail.fr>
2018-10-15 21:43:33 +0200

    Create 6_boucles.py

---
+++ Tutoriels/Python/Python/Additional/Exemples/6_boucles.py
@@ -0,0 +1,27 @@
+##### dia 164 #####
+nombre = 2
+
+while nombre < 5 :
+	print nombre
+	nombre = nombre+1
+
+print "Fin while, nombre :", nombre
+
+
+##### dia 166 #####
+nombre = 2
+
+while nombre < 5 :
+	print nombre
+
+
+##### dia 172 #####
+for i in range(10):
+	print i
+	
+
+##### dia 173 #####
+mots = ['Ceci', 'est', 'une', 'suite', 'de', 'mots', 'affichés', 'en', 'colonne']
+
+for i in mots :
+	print i
