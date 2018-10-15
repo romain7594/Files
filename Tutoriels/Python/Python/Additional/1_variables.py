91ab84d7d2b0be26aa29e71df399a623fc32ddb5
romain7594 <ultimategseed2@hotmail.fr>
2018-10-15 20:25:13 +0200

    Delete 1_variables.py

--- Tutoriels/Python/Python/Additional/1_variables.py
+++
@@ -1,9 +0,0 @@
-#dia 21
-JeMetsDansLaVariableLeChiffreUn = 1
-JeContientUneChaineDeCaracteres = 'tototatatititutu'
-
-#dia 23
-mot1, mot2, mot3 = 'Coucou', 'ça', 'va'
-
-#dia 24
-phrase = 'Coucou', 'ça', 'va'
