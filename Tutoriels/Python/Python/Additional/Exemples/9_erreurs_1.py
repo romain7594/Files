8a79051d74f3f9f448bebdab4bf8f04fe6a8d9ce
romain7594 <ultimategseed2@hotmail.fr>
2018-10-15 22:52:38 +0200

    Create 9_erreurs_1.py

---
+++ Tutoriels/Python/Python/Additional/Exemples/9_erreurs_1.py
@@ -0,0 +1,105 @@
+##### dia 246 #####
+print 12/0
+
+
+##### dia 247 #####
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+print a
+
+
+##### dia 248 #####
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+math.log(67)
+
+
+
+##### dia 249 #####
+nombre = 9
+
+if nombre == 9 :
+print "oui"
+
+
+
+##### dia 250 #####
+variable = 378687689
+
+	print variable
+
+
+
+##### dia 251 #####
+nombre = (1+1)*2)/10
+
+
+
+##### dia 252 #####
+mot = "bonjour'
+
+
+##### dia 253 et 254 #####
+#!/usr/bin/python
+
+print "dkfkdhfèss"
+
+
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+print "dkfkdhfèss"
+
+
+##### dia 255 #####
+mot = 'mot'
+nombre = 8
+
+tout = mot+nombre
+
+
+
+##### dia 256 #####
+def carre(nombre):
+	return nombre*nombre
+	
+print carre(1, 2)
+
+
+##### dia 257 #####
+liste = [1, 2, 3, 4, 5]
+
+print liste[5]
+
+
+##### dia 258 #####
+fichier = open('truc.txt', 'r')
+
+
+##### dia 259 #####
+liste = [1, 2, 3]
+
+liste.remove(4)
+
+
+##### dia 260 #####
+UnTuple = (1, 2, 3)
+
+UnTuple.remove(1)
+
+
+
+##### dia 261 #####
+def CARRE(nombre):
+	truc
+	return nombre*nombre
+	
+def CUBE(nombre):
+	return CARRE(nombre)*nombre
+	
+def main(nombre):
+	return CUBE(nombre)
+
+main(2)
