a7f446572fadec0f804a3fb8c213e4ade7f6255c
romain7594 <ultimategseed2@hotmail.fr>
2018-10-15 22:11:53 +0200

    Create 8_bibliotheques_utiles.py

---
+++ Tutoriels/Python/Python/Additional/Exemples/8_bibliotheques_utiles.py
@@ -0,0 +1,17 @@
+##### dia 225 #####
+import math
+
+racine_carre = math.sqrt(9)
+logarithme = math.log(0.87687)
+exponentielle = math.exp(1)
+cosinus = math.cos(math.pi)
+
+print racine_carre
+print logarithme
+print exponentielle
+print cosinus
+
+
+##### dia 227 #####
+import random
+
