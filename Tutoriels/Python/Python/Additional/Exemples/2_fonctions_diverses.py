4a7280c8080e9fc4ccf0208c131d40c43f9abc05
romain7594 <ultimategseed2@hotmail.fr>
2018-10-15 20:30:37 +0200

    Create 2_fonctions_diverses.py

---
+++ Tutoriels/Python/Python/Additional/Exemples/2_fonctions_diverses.py
@@ -0,0 +1,34 @@
+### dia 35
+numero1 = 1
+mot = 'Bonjour'
+
+print numero1
+print mot
+print 'Turlututu'
+print '3690
+
+print numero1, mot, 9789687, 'dsfkljdslkfj'
+
+
+### dia 37
+untruc = raw_input()
+n = input("Un nombre : ")
+lettre = raw_input("Une lettre : ")
+phrase = raw_input("Une phrase : ")
+
+print untruc
+print n
+print lettre
+print phrase
+
+
+### dia 39
+type_int = 14
+type_float = 1.2
+type_str = 'coucou'
+unknown_type = type(1868768681)
+
+print type(type_int)
+print type(type_float)
+print type(type_str)
+print type(unknown_type)
