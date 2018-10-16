c1e688d4aea0b099bf4901889ff5b80f40274248
romain7594 <ultimategseed2@hotmail.fr>
2018-10-16 09:46:32 +0200

    Create 1_matrices.py

---
+++ Tutoriels/Python/Numpy/Additional/Exemples/1_matrices.py
@@ -0,0 +1,23 @@
+##### dia 11 #####
+p = [[1,2], [2,3]]
+P = numpy.array(p)
+
+print type(p)
+print p
+
+print type(P)
+print P
+
+
+##### dia 13 #####
+p = numpy.zeros((3,4))
+print p
+
+p = numpy.ones((2,2))
+print p
+
+p = numpy.full((5, 4), 9)
+print p
+
+p = numpy.eye(4)
+print p
