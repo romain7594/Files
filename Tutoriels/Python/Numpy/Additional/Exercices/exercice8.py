6672c86da31e4a788a3602e67759ff2f9eff8f81
romain7594 <ultimategseed2@hotmail.fr>
2018-10-17 14:43:14 +0200

    Create exercice8.py

---
+++ Tutoriels/Python/Numpy/Additional/Exercices/exercice8.py
@@ -0,0 +1,27 @@
+import numpy
+
+a1 = numpy.array([  [1, 1],
+		    [2, 5]  ])
+		  
+b1 = numpy.array([3, 12])
+
+S1 = numpy.linalg.solve(a1, b1)
+
+print "Solution :"
+print "x :", S1[0]
+print "y :", S1[1]
+
+
+
+a2 = numpy.array([  [2, 1, 4],
+		     [4, -2, 3],
+		     [8, 3, 2]  ])
+		    
+b2 = numpy.array([16, 9, 20])
+
+S2 = numpy.linalg.solve(a2, b1)
+
+print "Solution :"
+print "x :", S2[0]
+print "y :", S2[1]
+print "z :", S2[2] 
