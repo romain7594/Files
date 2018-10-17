d0233cd7f5a71f55f13699b3b1a9598ee2bdb106
romain7594 <ultimategseed2@hotmail.fr>
2018-10-17 12:57:23 +0200

    Create 8_erreur.py

---
+++ Tutoriels/Python/Python/Additional/Exercices/8_erreur.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import math
+
+variableé = 100
+	variable2 = 500
+	variable3 = 13.8
+	
+print math.log(variable1, 10)
+
+for i in range(20)
+	variable1 = variable+10
+	
+	if variable1 == 200:
+	print "Supérieur à 200"
+	
+	
+if variable2 > 300:
+	print 'Oh bien !!!"
+	variable3 = variable2+variable3
+
+else:
+	print "Peut mieux faire'
+	
+valeur_aleatoire = random.random()
+print valeur_aleatoire
+
+print Vous avez reussi !!!!"
