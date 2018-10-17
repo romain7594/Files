1aeb2602f3484cfb7c6b91dd14e8156cd4eb2b7a
romain7594 <ultimategseed2@hotmail.fr>
2018-10-17 14:08:16 +0200

    Create exercice2.py

---
+++ Tutoriels/Python/Numpy/Additional/Exercices/exercice2.py
@@ -0,0 +1,40 @@
+import numpy
+
+M = numpy.array([11, 12, 15], [10, 11, 32], [40, 18, 10]])
+dico_indice = {}
+i = 0
+
+for i in range(len(M)) :
+	for j in range(len(M[i])) :
+		dico_indice[[i, j] = i
+		i += 1
+		
+indice_nombre = dico_indice.items()
+
+dico_indice = {}
+
+for i in indice_nombre :
+	dico_indice[i[1]] = i[0]
+
+
+		
+mini = numpy.argmin(M)
+maxi = numpy.argmax(M)
+
+val_mini = M[dico_indice[mini][0], dico_indice[mini][1]]
+val_maxi = M[dico_indice[maxi][0], dico_indice[maxi][1]]
+
+indice_mini = []
+indice_maxi = []
+
+for i in range(M.shape[0]):
+	for j in range(M.shape[1]):
+		if M[i,j] == val_mini :
+			indice_mini.append([i,j])
+		
+		if M[i,j] == val_maxi :
+			indice_maxi.append([i,j])
+			
+print M
+print indice_mini
+print indice_mini
