1b1c3c30ad3ac48a2cbbb10c496baa0131b4c458
romain7594 <ultimategseed2@hotmail.fr>
2018-10-17 13:02:06 +0200

    Create 8_erreur_correct.py

---
+++ Tutoriels/Python/Python/Additional/Exercices/8_erreur_correct.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import math
+import random                                  ######################## ajout de import random
+
+variable1 = 100                                ######################## pas d'accent dans un nom de variable
+variable2 = 500                                ########
+variable3 = 13.8                               ######## indentation non nécessaire
+	
+print math.log(variable1, 10)
+
+for i in range(20):                            #######  ":" à la fin d'un for
+	variable1 = variable+10
+	
+	if variable1 == 200:
+		print "Supérieur à 200"        ####### indentation
+	
+	
+if variable2 > 300:
+	print "Oh bien !!!"                    ####### " à la place de ' avant Oh
+	variable3 = variable2+variable3
+
+else:
+	print "Peut mieux faire"
+	
+valeur_aleatoire = random.random()             ####### import random au début, ou avant cette ligne
+print valeur_aleatoire
+
+print "Vous avez reussi !!!!"                  ####### Oublié d'un " au début de la chaine
