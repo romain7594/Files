25b3a86d3209731388017b39c01ca0bb32a73183
romain7594 <ultimategseed2@hotmail.fr>
2018-10-18 14:12:40 +0200

    Update exercice4.py

--- Tutoriels/Python/Matplotlib/Additional/Exercices/exercice4.py
+++ Tutoriels/Python/Matplotlib/Additional/Exercices/exercice4.py
@@ -3,14 +3,16 @@ import matplotlib.pyplot as plt
 v = [35.2, 37.4, 26.4, 10.6, 111.2, 64, 48.2, 37, 49.1, 43.4, 69.7, 29]
 X = range(12)
 
-plt.bar(X, v, align="center")
+plt.bar(X, v)
 
-plt.xticks(["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"], rotation=45)
+plt.xticks(range(12), ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"], rotation=20)
 
 plt.xlabel("Mois")
 plt.ylabel("Precipitations (mm)")
-plt.title("Precipitations au cours de l'annee 2013")
+plt.title("Precipitations mensuelles au cours de l'annee 2013")
 
-#plt.plot(X, v, "o")
+plt.grid(axis='y', linestyle="-", alpha=0.5)
+
+#plt.plot([i+0.4 for i in X], v, marker="o", color="black")
 
 plt.show()
