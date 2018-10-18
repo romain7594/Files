1aa02c3b47dfa2d94a25150bab36e9a2378a93c4
romain7594 <ultimategseed2@hotmail.fr>
2018-10-18 15:06:25 +0200

    Update exercice3.py

--- Tutoriels/Python/Numpy/Additional/Exercices/exercice3.py
+++ Tutoriels/Python/Numpy/Additional/Exercices/exercice3.py
@@ -4,7 +4,7 @@ M = numpy.array([   [11, 12, 15],
 		     [10, 11, 32],
 		     [40, 18, 10]   ])
 
-tmp = numpy.vsplit(M, [1:])
+tmp = numpy.vsplit(M, [1])
 
 premiere_ligne = tmp[0]
 lignes_suivantes = tmp[1]
