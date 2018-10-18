c469ed535f340ffb897cb6bb074552e44067f713
romain7594 <ultimategseed2@hotmail.fr>
2018-10-18 14:00:28 +0200

    Update exercice1.py

--- Tutoriels/Python/Matplotlib/Additional/Exercices/exercice1.py
+++ Tutoriels/Python/Matplotlib/Additional/Exercices/exercice1.py
@@ -5,15 +5,21 @@ carre = lambda x : x*x
 cube = lambda x : math.pow(x, 3)
 cosinus = lambda x : math.cos(x)
 racine = lambda x : math.sqrt(x)
-logarithme = lambda x : math.log10(x)
 
-X = range(100)
+plt.ylim(-15, 15)
+plt.xlim(-15, 15)
 
+X = range(-15, 15)
+XX = range(15)
+
+plt.plot(X, X, color="k", marker="o", label="x = x")
 plt.plot(X, [carre(i) for i in X], color="red", label="carre")
 plt.plot(X, [cube(i) for i in X], color="blue", linestyle="dotted", label="cube")
 plt.plot(X, [cosinus(i) for i in X], color="green", linestyle="dashed", label="cosinus")
-plt.plot(X [racine(i) forn i in X], color="yellow", linestyle="dashdot", label="racine")
-plt.plot(X, [logarithme(i) for i in X], color="k", marker="o", label="logarithme")
+plt.plot(XX, [racine(i) for i in XX], color="black", linestyle="dashdot", label="racine")
+
+plt.axvline(0, color="k", alpha=0.3)
+plt.axhline(0, color="k", alpha=0.3)
 
 plt.legend()
 plt.show()
