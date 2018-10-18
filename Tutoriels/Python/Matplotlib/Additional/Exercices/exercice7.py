4cf5ffaf85e12d8e3c68faa81353f91c9b064d09
romain7594 <ultimategseed2@hotmail.fr>
2018-10-18 14:23:48 +0200

    Update exercice7.py

--- Tutoriels/Python/Matplotlib/Additional/Exercices/exercice7.py
+++ Tutoriels/Python/Matplotlib/Additional/Exercices/exercice7.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 prop = [50, 30, 10, 5, 5]
 ingredients = ["Pate", "Sauce tomate", "Fromage", "Jambon", "Champignons"]
 
-plt.pie(prop, labels=ingredients, colors=["lightgoldenrod", "orangered", "coral", "lightpink", "azure"], autopct='%.f%%')
+plt.pie(prop, labels=ingredients, colors=["gold", "orangered", "coral", "lightpink", "azure"], autopct='%.f%%')
 
 plt.title("Pizza jambon fromage champignons")
 
