f318a6b55c707dfc433b40bfc80fa9e687451994
romain7594 <ultimategseed2@hotmail.fr>
2018-10-18 15:10:13 +0200

    Update exercice5.py

--- Tutoriels/Python/Numpy/Additional/Exercices/exercice5.py
+++ Tutoriels/Python/Numpy/Additional/Exercices/exercice5.py
@@ -5,14 +5,14 @@ M = numpy.array([  [11, 20, 18, 98],
 		    [17, 39, 31, 58],
 	 	    [24, 71, 70, 87]  ])
 		     
-col_split = numpy.hsplit(M, [2:])
-new = numpy.hstack(col_split[1], col_split[0])
+col_split = numpy.hsplit(M, [2])
+new = numpy.hstack((col_split[1], col_split[0]))
 
 print M
 print new
 
-row_split = numpy.vsplit(new, [2:])
-new2 = numpy.vstack(row_split[1], row_split[0])
+row_split = numpy.vsplit(new, [2])
+new2 = numpy.vstack((row_split[1], row_split[0]))
 
 print new
 print new2
