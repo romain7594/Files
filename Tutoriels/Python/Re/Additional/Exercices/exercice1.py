307a17a73adf5b43749223fdb009d86a8092b3c5
romain7594 <ultimategseed2@hotmail.fr>
2018-10-18 10:33:39 +0200

    Update exercice1.py

--- Tutoriels/Python/Re/Additional/Exercices/exercice1.py
+++ Tutoriels/Python/Re/Additional/Exercices/exercice1.py
@@ -4,4 +4,6 @@ motif = re.compile("^\w{5}\s\w{2}\s\d\d\.\d\d\.\d\s[a-z]*\s[a-z]*\.\d\d\s\w*$")
 
 c = "START NB 12.56.7 silhouette marche.49 fin"
 
-print motif.search(c)
+r = motif.search(c)
+
+print r
