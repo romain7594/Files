0dff5ffcbc46f0671cb7a8cf0e8d43f0d46d93b3
romain7594 <ultimategseed2@hotmail.fr>
2018-11-29 22:44:43 +0100

    Create 19_diapo_59.py

---
+++ Tutoriels/Python/Python POO/Additionnal/Exemples/19_diapo_59.py
@@ -0,0 +1,16 @@
+class Exemple(object):
+	def __setattr__(self, attr, val):
+		self.attr = val
+		
+E = Exemple()
+E.element= "voiture"
+
+###################################################
+
+class Exemple(object):
+	def __setattr__(self, attr, val):
+		print("L'attribut {} a bien ete cree".format(attr))
+		return object.__setattr__(self, attr, val)
+		
+E = Exemple()
+E.element= "voiture"
