8ded2b29673d8289e0c5de2b89c7258ad98c72b3
romain7594 <ultimategseed2@hotmail.fr>
2018-11-29 23:33:43 +0100

    Delete 21_diapo_61.py

--- Tutoriels/Python/Python POO/Additionnal/Exemples/21_diapo_61.py
+++
@@ -1,9 +0,0 @@
-class Exemple(object):
-	def __init__(self):
-		self.objet = "Diamant"
-		
-	def __setattr__(self, attribut, valeur):
-		raise AttributeError("Modification impossible")
-		
-E = Exemple()
-E.objet = "Je vous l'emprunte"
