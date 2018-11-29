1907b32763b3efa7444eb8f6d78e77cd506bd905
romain7594 <ultimategseed2@hotmail.fr>
2018-11-29 23:42:39 +0100

    Update 7_heritage_simple.py

--- Tutoriels/Python/Python POO/Additionnal/Exemples/7_heritage_simple.py
+++ Tutoriels/Python/Python POO/Additionnal/Exemples/7_heritage_simple.py
@@ -1,23 +1,23 @@
 ##### dia 72 #####
 class A(object):
-	pass
+        pass
 
 class B(A):
-	pass
+        pass
 
 
 ##### dia 73 #####
 class A(object):
-	def __init__(self):
-		self.a = 1
-		self.b = 2
-	
-	def affiche(self):
-		print(self.a, self.b)
-		
+        def __init__(self):
+                self.a = 1
+                self.b = 2
+
+        def affiche(self):
+                print(self.a, self.b)
+
 class B(A):
-	pass
-	
+        pass
+
 P = B()
 print(P.a, P.b)
 P.affiche()
@@ -25,36 +25,36 @@ P.affiche()
 
 ##### dia 74 et 75 #####
 class Cylindre(object):
-	def __init__(self, hauteur, rayon):
-		self.hauteur = hauteur
-		self.rayon = rayon
-		
-	def aire(self):
-		A = 2*3.14*self.rayon*self.hauteur+2*3.14*(self.rayon**2)
-		return A
-		
-	def volume(self):
-		V = 3.14*(self.rayon**2)*self.hauteur
-		return V
+        def __init__(self, hauteur, rayon):
+                self.hauteur = hauteur
+                self.rayon = rayon
+
+        def aire(self):
+                A = 2 * 3.14 * self.rayon * self.hauteur + 2 * 3.14 * (self.rayon ** 2)
+                return A
+
+        def volume(self):
+                V = 3.14 * (self.rayon ** 2) * self.hauteur
+                return V
 
 class Bouteille(Cylindre):
-	def __init__(self, hauteur, rayon):
-		self.hauteur = hauteur
-		self.rayon = rayon
-		
+        def __init__(self, hauteur, rayon):
+                self.hauteur = hauteur
+                self.rayon = rayon
+
 
 ##### dia 77 #####
 class UnObjet(object):
-	def affiche(self):
-		print("Hello")
-		
+        def affiche(self):
+                print("Hello")
+
 class UnTruc(UnObjet):
-	def affiche(self):
-		print("Bonjour")
-		
+        def affiche(self):
+                print("Bonjour")
+
 class UneChose(UnObjet):
-	pass
-	
+        pass
+
 A = UnObjet()
 B = UnTruc()
 C = UneChose()
@@ -66,54 +66,54 @@ C.affiche()
 
 ##### dia 78 #####
 class Bouteille(Cylindre):
-	def __init__(self, hauteur, rayon):
-		Cylindre.__init__(self, hauteur, rayon)
-		
+        def __init__(self, hauteur, rayon):
+                Cylindre.__init__(self, hauteur, rayon)
+
 T = Bouteille(6, 4)
 
-print(T.Rayon)
+print(T.rayon)
 print(T.hauteur)
-		
+
 
 ##### dia 79 #####
 class Bouteille(Cylindre):
-	def __init__(self, hauteur, rayon):
-		Cylindre.__init__(self, hauteur, rayon)
-		self.matiere = "plastique"
-		self.contenu = "eau gazeuse"
-		
+        def __init__(self, hauteur, rayon):
+                Cylindre.__init__(self, hauteur, rayon)
+                self.matiere = "plastique"
+                self.contenu = "eau gazeuse"
+
 class RouleauDePapierToilette(Cylindre):
-	def __init__(self, hauteur, rayon):
-		Cylindre.__init__(self, hauteur, rayon)
-		self.poids = 200
-		self.utilisation = "hygiene"
-		
+        def __init__(self, hauteur, rayon):
+                Cylindre.__init__(self, hauteur, rayon)
+                self.poids = 200
+                self.utilisation = "hygiene"
+
 T = Bouteille(6, 4)
 print("Bouteille en {} contenant de l'{}".format(T.matiere, T.contenu))
 
-R = RouleauDePapierToilette()
+R = RouleauDePapierToilette(4, 2)
 print("Rouleau de papier toilette pesant {}g et ayant une fonction d'{}".format(R.poids, R.utilisation))
 
 
 ##### dia 81 et 82 #####
 class Cylindre(object):
-	def __init__(self, hauteur, rayon):
-		self.hauteur = hauteur
-		self.rayon = rayon
+        def __init__(self, hauteur, rayon):
+                self.hauteur = hauteur
+                self.rayon = rayon
 
-	def volume(self, h):
-		V = 3.14*(self.rayon**2)*h
-		return V
+        def volume(self, h):
+                V = 3.14 * (self.rayon ** 2) * h
+                return V
 
 class Bouteille(Cylindre):
-	def __init__(self, hauteur, rayon):
-		Cylindre.__init__(self, hauteur-hauteur//4, rayon)
-		self.hauteur_cone = hauteur//4
-		
-	def volume(self):
-		V = super(Bouteille, self).volume(self.hauteur)
-		C = super().volume(self.hauteur_cone)/3
-		return C+V
-		
+        def __init__(self, hauteur, rayon):
+                Cylindre.__init__(self, hauteur - hauteur // 4, rayon)
+                self.hauteur_cone = hauteur // 4
+
+        def volume(self):
+                V = super(Bouteille, self).volume(self.hauteur)
+                C = super().volume(self.hauteur_cone) / 3
+                return C + V
+
 T = Bouteille(8, 2)
 print(T.volume())
