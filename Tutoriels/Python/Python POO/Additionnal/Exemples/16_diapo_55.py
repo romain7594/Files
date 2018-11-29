28025898f2748bea7ca584da6aa741f140186cee
romain7594 <ultimategseed2@hotmail.fr>
2018-11-29 22:36:46 +0100

    Create 16_diapo_55.py

---
+++ Tutoriels/Python/Python POO/Additionnal/Exemples/16_diapo_55.py
@@ -0,0 +1,15 @@
+class Personne(object):
+	def __init__(self, prenom, taille):
+		self.prenom = prenom
+		self.taille = taille
+		
+	def __lt__(self, P):
+		if self.taille > P.taille :
+			return True
+		else :
+			return False
+			
+P = Personne("Bernard", 170)
+P2 = Personne'"Gregoire", 176)
+
+print(P < P2)
