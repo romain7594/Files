cd030a151923f0130fd34a5c971f49639e462fd7
romain7594 <ultimategseed2@hotmail.fr>
2018-11-29 15:47:36 +0100

    Create bouteille.py

---
+++ Tutoriels/Python/Python POO/Exercices/bouteille/bouteille.py
@@ -0,0 +1,23 @@
+class Cylindre(object):
+	def __init__(self, hauteur, rayon):
+                self.hauteur = hauteur
+                self.rayon = rayon
+
+        def volume(self, h):
+                V = 3.14*(self.rayon**2)*h
+                return V
+
+
+class ActionRecipient(object):
+        pass
+
+
+class Bouteille(Cylindre, ActionRecipient):
+        def __init__(self, hauteur, rayon):
+                Cylindre.__init__(self, hauteur-hauteur//4, rayon)
+                self.hauteur_cone = hauteur//4
+
+        def volume(self):
+                V = super().volume(self.hauteur)
+                C = super().volume(self.hauteur_cone)/3
+                return C+V
