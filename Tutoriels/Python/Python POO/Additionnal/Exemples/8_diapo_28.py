e5f43318ed4631a1a91ed8831097c08bb1e50118
romain7594 <ultimategseed2@hotmail.fr>
2018-11-29 21:32:43 +0100

    Create 8_diapo_28.py

---
+++ Tutoriels/Python/Python POO/Additionnal/Exemples/8_diapo_28.py
@@ -0,0 +1,9 @@
+class Point(object):
+	"""Classe Point contenant les coordonnees x et y"""
+	def __init__(self):
+		"""
+		Methode d'initialisation de la classe Point
+		Coordonnees x et y
+		"""
+		self.x = 1
+		self.y = 2
