882bca1bdb63e362241a6e4b8e0a94867a1685a5
romain7594 <ultimategseed2@hotmail.fr>
2018-11-29 23:18:02 +0100

    Update and rename 1_diapo_5.py to introduction.py

--- Tutoriels/Python/Python POO/Additionnal/Exemples/1_diapo_5.py
+++
@@ -1,5 +0,0 @@
-date = 19
-print("Nous somme le {} octobre {}".format(date, 2018))
-
-T = "Il est actuellement {0}h{1}"
-print(T.format(18, 34))
---
+++ Tutoriels/Python/Python POO/Additionnal/Exemples/introduction.py
@@ -0,0 +1,10 @@
+##### dia 5 #####
+date = 19
+print("Nous somme le {} octobre {}".format(date, 2018))
+
+T = "Il est actuellement {0}h{1}"
+print(T.format(18, 34))
+
+
+##### dia 7 #####
+print("{sujet} {negation1}{verbe} {negation2}".format(verbe="arrive", negation2="pas", sujet="L'hiver", negation1="n'"))
